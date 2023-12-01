aa8b6ea453f4b62eab6340ee1d9a0ab810233237
caleb <calebfelix38@gmail.com>
2023-12-01 15:15:43 +0530

    first rewiew

--- app/(dashboard)/admin/page.js
+++ app/(dashboard)/admin/page.js
@@ -95,13 +95,16 @@ const adminpage = () => {
           >
             Policy
           </button>
-          {/* <button
+          <button
             className="text-white m-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             type="button"
-            onClick={handlesendEmail}
+            onClick={(e) => {
+              setIsLoading((prev) => true);
+              router.push(`/dummy`);
+            }}
           >
-            send Email
-          </button> */}
+            statistics
+          </button>
         </div>
       </div>
     </>
