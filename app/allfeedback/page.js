522ab43579201945205ed3b014c51a7bae1b00f2
rohit verma <rohitverma1569@gmail.com>
2023-12-01 14:13:24 +0530

    added feedback

---
+++ app/allfeedback/page.js
@@ -0,0 +1,13 @@
+
+import AllFeedBack from '@/components/allFeedback/AllFeedback'
+import React from 'react'
+
+const allFeedback = () => {
+  return (
+    <>
+      <AllFeedBack />
+    </>
+  )
+}
+
+export default allFeedback
